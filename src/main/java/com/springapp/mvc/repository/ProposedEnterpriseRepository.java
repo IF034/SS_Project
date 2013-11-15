b64b53655df9260a7c1fb2cbf2884e5b4304e169
vchikatc <softserbediscount@ukr.net>
2013-11-15 07:11:22 -0800

    drop for new revision

--- src/main/java/com/springapp/mvc/repository/ProposedEnterpriseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springapp.mvc.repository;
-
-import com.springapp.mvc.entity.ProposedEnterprise;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProposedEnterpriseRepository extends JpaRepository<ProposedEnterprise, Integer> {
-}
--- src/main/java/com/springapp/mvc/service/CommentRatioService.java
+++
@@ -1,9 +0,0 @@
-package com.springapp.mvc.service;// Created with IntelliJ IDEA by Yaroslav Kovbas (Xardas)
-
-import com.springapp.mvc.entity.CommentRatio;
-
-public interface CommentRatioService {
-
-    public void save(CommentRatio commentRatio);
-    public Boolean isCommentRatedByUser(int userId, int commentId);
-}
